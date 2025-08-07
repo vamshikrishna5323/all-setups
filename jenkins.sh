378e4fc6e8f304fdb8db5cb6f16608de4036bb08
vamshikrishna5323 <kanagantivamshikrishna@gmail.com>
2025-08-07 15:20:26 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -11,6 +11,16 @@ yum install java-17-amazon-corretto -y
 yum install jenkins -y
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
 systemctl enable jenkins.service
+systemctl start jenkins.service
 systemctl status jenkins.service
+
+
+#Build executors offile to online - to fix the issue by using below cmd's 
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
